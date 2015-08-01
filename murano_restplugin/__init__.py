470fb434fac777b6a17e8fe38cf44e4708695fe3
Serg Melikyan <smelikyan@mirantis.com>
2015-07-31 17:55:16 -0700

    Make plugin working
    * Add entry_points section to the setup.py
    * Remove setup.cfg
    * Inherit plugin from object instead of MuranoObject
    
    Essentially only missing entry_points section in the setup.py was preventing
    this plugin from working.
    
    File setup.cfg was not used at all, and inheritance from MuranoObject may be safely skiped.

--- murano_restplugin/__init__.py
+++ murano_restplugin/__init__.py
@@ -1,6 +1,3 @@
-import murano.dsl.murano_object as murano_object
-
-
-class REST(murano_object.MuranoObject):
-	def initialize(self, _context, debug):
-		pass
+class REST(object):
+    def initialize(self, _context, debug):
+        pass
--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from distutils.core import setup
 
 setup(
     name='murano_restplugin',
-    version='0.1.14',
+    version='0.1.15',
     packages=['murano_restplugin'],
     url='http://wiki.web.att.com/pages/viewpage.action?pageId=431763028',
     license='',
     author='OL7435',
     author_email='ol7435@att.com',
-    description='Murano plugin to enable REST calls from Murano PL classes'
+    description='Murano plugin to enable REST calls from Murano PL classes',
+    entry_points={
+        'io.murano.extensions': [
+            'att.rest=murano_restplugin:REST',
+        ],
+    },
 )
